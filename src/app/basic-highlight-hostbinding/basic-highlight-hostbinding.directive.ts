1fc62f44cb7d81575b15f341be8d1bda5a6a6786
gr33ngr33n <actfun07@gmail.com>
2018-06-01 00:18:11 +0800

    create directive properties

--- src/app/basic-highlight-hostbinding/basic-highlight-hostbinding.directive.ts
+++ src/app/basic-highlight-hostbinding/basic-highlight-hostbinding.directive.ts
@@ -1,20 +1,28 @@
-import {Directive, HostBinding, HostListener} from '@angular/core';
+import {Directive, HostBinding, HostListener, Input, OnInit} from '@angular/core';
 
 @Directive({
   selector: '[appBasicHighlightHostbinding]'
 })
 
-export class BasicHighlightHostbindingDirective {
-  @HostBinding('style.backgroundColor') colorOfBack: string = 'blue';
+export class BasicHighlightHostbindingDirective implements OnInit {
+  @HostBinding('style.backgroundColor') colorOfBack: string;
+  @Input() initialColor: string;
+  @Input() highlightColor: string;
+  // @Input('appBasicHighlightHostbinding') highlightColor: string;  // use alias similar to selector if this is the main/ the only property
+
 
   constructor() {
   }
 
+  ngOnInit() {
+    this.colorOfBack = this.initialColor;
+  }
+
   @HostListener('mouseenter') onMouseOver() {
-    this.colorOfBack = 'orange';
+    this.colorOfBack = this.highlightColor;
   }
 
   @HostListener('mouseleave') onMouseLeave() {
-    this.colorOfBack = 'blue';
+    this.colorOfBack = this.initialColor;
   }
 }
