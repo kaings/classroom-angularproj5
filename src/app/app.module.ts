4f92ccbe3957fe7ef258d394dea45e012448e501
gr33ngr33n <actfun07@gmail.com>
2018-05-31 22:51:06 +0800

    directive using renderer2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
+import { BasicHighlightRenderer2Directive } from './basic-highlight-renderer2/basic-highlight-renderer2.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BasicHighlightDirective
+    BasicHighlightDirective,
+    BasicHighlightRenderer2Directive
   ],
   imports: [
     BrowserModule
