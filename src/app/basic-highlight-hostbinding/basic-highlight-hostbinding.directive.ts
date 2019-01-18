3c66c9b58358a9223c3ba52c50621044e1349367
willys_tjiawi <willys@huubap.com>
2019-01-18 13:47:24 +0800

    add class via @HostBinding triggered by @HostListener event

--- src/app/basic-highlight-hostbinding/basic-highlight-hostbinding.directive.ts
+++ src/app/basic-highlight-hostbinding/basic-highlight-hostbinding.directive.ts
@@ -6,15 +6,18 @@ import {Directive, HostBinding, HostListener} from '@angular/core';
 
 export class BasicHighlightHostbindingDirective {
   @HostBinding('style.backgroundColor') colorOfBack: string = 'blue';
+  @HostBinding('class.on-mouse-over') isOnMouseOver: boolean = false;
 
   constructor() {
   }
 
   @HostListener('mouseenter') onMouseOver() {
     this.colorOfBack = 'orange';
+    this.isOnMouseOver = true;
   }
 
   @HostListener('mouseleave') onMouseLeave() {
     this.colorOfBack = 'blue';
+    this.isOnMouseOver = false;
   }
 }
